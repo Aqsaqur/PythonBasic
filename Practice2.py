50f41aa90455d45c3394881b84919398588874ae
Aqsa Qur <aqsaqurayshi@gmail.com>
2025-03-13 07:04:34 +0500

    practice somequestions

---
+++ Practice2.py
@@ -0,0 +1,34 @@
+# write a program wap if a number entered by the user is odd or even 
+
+# num = int(input("enter number: "))
+
+# if num % 2 == 0:
+#     print("the number is even")
+# else:
+#     print("the number is odd")
+
+# write the above program in nested loops
+
+
+
+# wap to find the greatest of 3 numbers entered by the user.
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+c = int(input("enter the third number: "))
+
+if a >= b and a >= c:
+    print("the first number is largest")
+elif b >= c:
+    print("second is the largest")
+else:
+    print("third is the largest number")
+
+
+# wap to check if a number is a multipule of 7 or not
+x = int(input("enter number: "))
+
+if x % 7 == 0:
+    print("multiple of 7")
+else:
+    print("not a multiple")
+
